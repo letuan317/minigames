e8e7f2df1f86d562873dde08069e7f57d32928da
letuan317 <letuan317@gmail.com>
2021-07-30 17:51:27 -0700

    update heroku

--- client/src/socketio.js
+++ client/src/socketio.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const url_proxy = "http://24.7.115.67:8000/";
+const url_proxy = "http://localhost:8000/";
 
 export const socket = io.connect(url_proxy, {
   transports: ["websocket", "polling", "flashsocket"],
